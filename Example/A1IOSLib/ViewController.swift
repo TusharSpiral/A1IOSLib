7c57c302dd8586e3409e7f76f419a8586721bfc5
“Your <TusharSpiral@github.com>
2023-09-08 15:52:59 +0530

    Initial commit

---
+++ Example/A1IOSLib/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  A1IOSLib
+//
+//  Created by TusharSpiral on 09/08/2023.
+//  Copyright (c) 2023 TusharSpiral. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
